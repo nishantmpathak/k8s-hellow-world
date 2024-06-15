24b3bef44e1f7f6413cfe5a8de093b2d415e286e
nishantpathak <nishantpathak97@gmail.com>
2024-06-15 18:49:14 +0530

    First Commit

--- README.md
+++ README.md
@@ -24,3 +24,6 @@ Open a browser and go to http://<minikube-ip>:30007/hello.
 Above did not work for me I need make tunneling below command worked for me. It started browser with some new IP id
 Sometimes Minikube may need a tunnel to expose services. Run:
 minikube service demo-spring-boot-app
+
+If we do any change in config-map, we need redeploy the pods to see changes
+kubectl rollout restart deployment demo-spring-boot-app
--- src/main/java/com/example/controller/RestController.java
+++ src/main/java/com/example/controller/RestController.java
@@ -1,5 +1,6 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +10,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/hello-world")
 public class RestController {
 
+    @Value("${my.custom.property}")
+    private String value;
     @GetMapping
     public ResponseEntity<String> getMessage(){
-        return ResponseEntity.status(HttpStatus.OK).body("Hello World");
+        return ResponseEntity.status(HttpStatus.OK).body("Hello World "+value);
     }
 
 }
